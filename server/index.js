6c7e0501fe55b616cd796f761f2da0f5e9bd3a6c
Varniha <91463219+varniha@users.noreply.github.com>
2023-11-30 12:46:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connection();
 app.use(express.json());
 app.use(cors(
   {
-    origin: [https://authentication-mern-stack-k2t3.vercel.app"],
+    origin: [https://authentication-mern-stack-api.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }          
